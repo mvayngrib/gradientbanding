769515085f7abe2cdff13e39d7f6c6ce529c0ccf
Mark Vayngrib <mark@tradle.io>
2019-08-03 16:56:26 -0400

    isolate svg

--- App.js
+++ App.js
@@ -8,8 +8,8 @@
 
 import React, { Component } from 'react'
 import { Platform, StyleSheet, Text, View } from 'react-native'
-// import BgSVG from './BgSVG'
-import BgLG from './BgLG'
+import BgSVG from './BgSVG'
+// import BgLG from './BgLG'
 
 const instructions = Platform.select({
 	ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,7 +20,7 @@ type Props = {}
 
 export default class App extends Component<Props> {
 	render() {
-		return <BgLG />
+		return <BgSVG />
 	}
 }
 
